b1e679a84b6c4c83a6e29e367d929a52d6443fbb
Lusine Kenneth <lusinekenneth2@gmail.com>
2024-07-02 20:52:56 -0400

    started my blog with prismic

---
+++ src/components/Richtext.tsx
@@ -0,0 +1,34 @@
+import { RichTextField } from "@prismicio/client";
+import { JSXMapSerializer, PrismicRichText, PrismicLink } from "@prismicio/react";
+
+
+export const richTextComponenets : JSXMapSerializer = {
+    label: ({node, children}) => {
+        if(node.data.label === 'codespan'){
+            return <code>{children}</code>
+        }
+    },
+    heading1: ({children}) => (
+        <h1 className="text-4xl font-bold">{children}</h1>
+    ),
+    heading2: ({children}) => (
+        <h2 className="text-xl font-bold">{children}</h2>
+    ),
+    heading3: ({children}) => (
+        <h3 className="text-lg font-bold">{children}</h3>
+    ),
+    paragraph: ({children}) => (
+        <p>{children}</p>
+    ),
+    hyperlink: ({ children, node }) => (
+        <PrismicLink field={node.data} className="font-bold underline">{children}</PrismicLink>
+      ),
+};
+
+interface RichTextProps {
+    field: RichTextField;
+}
+
+export const RichText = ({ field }: RichTextProps) => {
+    return <PrismicRichText field={field} components={richTextComponenets} />;
+};
